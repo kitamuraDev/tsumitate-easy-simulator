9dad8bc69fb705d8ac31ccc549668fb142b2f6fb
kitamuraDev <masato1999824@icloud.com>
2024-01-21 00:31:04 +0900

    :sparkles: 任意の入力フォームを追加 #15

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,9 +30,21 @@ export class AppComponent implements OnInit {
   }
 
   inputs = new FormGroup({
-    amountReqired: new FormControl(3, [Validators.required, Validators.min(1), Validators.max(10)]),
+    amountRequired: new FormControl(3, [Validators.required, Validators.min(1), Validators.max(10)]),
     yearRequired: new FormControl(1, [
       Validators.required,
+      Validators.min(1),
+      Validators.max(40),
+      this.validationService.integerValueValidator(),
+    ]),
+    amountAny1: new FormControl(null, [Validators.min(1), Validators.max(10)]),
+    yearAny1: new FormControl(null, [
+      Validators.min(1),
+      Validators.max(40),
+      this.validationService.integerValueValidator(),
+    ]),
+    amountAny2: new FormControl(null, [Validators.min(1), Validators.max(10)]),
+    yearAny2: new FormControl(null, [
       Validators.min(1),
       Validators.max(40),
       this.validationService.integerValueValidator(),
@@ -40,17 +52,28 @@ export class AppComponent implements OnInit {
     rate: new FormControl(5, [Validators.required, Validators.min(0), Validators.max(20)]),
   });
 
+  // 入力イベントで発火するメソッド
   onInput() {
+    const resultAnyInput1 = this.validationService.isOneInputEmptyValidator(
+      this.inputs.value.amountAny1,
+      this.inputs.value.yearAny1,
+    );
+
+    const resultAnyInput2 = this.validationService.isOneInputEmptyValidator(
+      this.inputs.value.amountAny2,
+      this.inputs.value.yearAny2,
+    );
+
     // FormGroupにある全てのフォームのバリデーションが通れば `true` が返る
-    // [disabled]="" <- をfalseにするとボタン押下できるようになるため、`isAbnormalInput`に代入する際に反転させてる
-    this.isAbnormalInput = !this.inputs.valid;
+    // [disabled]="" <- をfalseにするとボタン押下できるようになる
+    this.isAbnormalInput = this.inputs.valid && resultAnyInput1 && resultAnyInput2 ? false : true;
   }
 
   setTsumitateInput() {
     this.tsumitate = {
       input: {
-        amounts: [this.inputs.value.amountReqired!],
-        years: [this.inputs.value.yearRequired!],
+        amounts: [this.inputs.value.amountRequired!, this.inputs.value.amountAny1!, this.inputs.value.amountAny2!],
+        years: [this.inputs.value.yearRequired!, this.inputs.value.yearAny1!, this.inputs.value.yearAny2!],
         rate: this.inputs.value.rate!,
       },
       output: {
