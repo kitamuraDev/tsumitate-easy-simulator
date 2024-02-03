67dd92a761441ba4f097bcc40e1b85bec2586ff2
kitamuraDev <masato1999824@icloud.com>
2024-02-04 00:44:29 +0900

    :sparkles: /overview ページを実装 #21

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  // { path: '', redirectTo: '/overview', pathMatch: 'full' },
+  { path: '', redirectTo: '/overview', pathMatch: 'full' },
+  {
+    path: 'overview',
+    loadComponent: () => import('./features/overview/overview.component'),
+  },
   {
     path: 'simulation',
     loadComponent: () => import('./features/simulation/simulation.component'),
