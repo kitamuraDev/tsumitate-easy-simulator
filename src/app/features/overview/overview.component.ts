98c67a947e410d28c5623dba70cef0718a919165
kitamuraDev <masato1999824@icloud.com>
2025-01-18 18:41:58 +0900

    :speech_balloon: 文言修正

--- src/app/features/overview/overview.component.ts
+++ src/app/features/overview/overview.component.ts
@@ -9,11 +9,13 @@ import { Router } from '@angular/router';
   imports: [HeadContentComponent, BaseButtonComponent],
   template: `
     <app-head-content [title]="'はじめに'" [hasDescription]="true">
-      <p>当シュミレーションでは、想定利回り○%で毎月○万円を○年間、積立投資をした場合の資産評価額を計算できます。</p>
       <p>
-        また、グレー背景にある"毎月積立額"と"積立期間"の入力は、「積立投資を行う中で
+        当資産運用シュミレーターでは、想定利回り○%で毎月○万円を○年間、積立投資をした場合の資産評価額を計算できます。
+      </p>
+      <p>
+        また、グレー背景にある"毎月積立額"と"積立期間"の入力により、「積立投資を行う中で
         <span class="underline font-semibold px-1">後から積立額を変更するパターン</span>
-        も考慮したシュミレーションを行いたい」といったケースに対応した入力項目です。
+        も考慮したシュミレーションを行いたい」ケースにも対応しています。
       </p>
     </app-head-content>
     <div class="grid place-content-center mt-10">
