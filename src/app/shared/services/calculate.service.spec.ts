4a9b2d414a09483a1d4c97ecd58ff2e10c388b65
kitamuraDev <masato1999824@icloud.com>
2025-08-13 13:45:09 +0900

    :test_tube: 既存のユニットテストを改善・強化

--- src/app/shared/services/calculate.service.spec.ts
+++ src/app/shared/services/calculate.service.spec.ts
@@ -8,7 +8,6 @@ import { CalculateService } from './calculate.service';
 
 describe('CalculateService', () => {
   let service: CalculateService;
-  const monthlyRate = 1.05 ** (1 / 12);
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -17,173 +16,128 @@ describe('CalculateService', () => {
     service = TestBed.inject(CalculateService);
   });
 
-  it('【複利計算】年利:5%, 初期資産:0万, 積立額:3万, 積立年数:3年', () => {
-    const initialAssetManYen = 0;
-    const monthlyAmounts = [30000];
-    const monthlyYears = [36];
-    const expectedCompoundInterest = 1160679;
+  const mockSimpleInterestCalcInput = [
+    {
+      initialAssetManYen: 0,
+      monthlyAmounts: [50000],
+      monthlyYears: [12],
+      expected: 600000,
+    },
+    {
+      initialAssetManYen: 0,
+      monthlyAmounts: [50000, 60000],
+      monthlyYears: [12, 24],
+      expected: 2040000,
+    },
+    {
+      initialAssetManYen: 0,
+      monthlyAmounts: [50000, 60000, 70000],
+      monthlyYears: [12, 24, 36],
+      expected: 4560000,
+    },
+    {
+      initialAssetManYen: 0,
+      monthlyAmounts: [50000, 60000, 70000, 80000],
+      monthlyYears: [12, 24, 36, 48],
+      expected: 8400000,
+    },
+  ];
 
-    const result = service['compoundInterestCalc'](initialAssetManYen, monthlyAmounts, monthlyYears, monthlyRate);
-
-    expect(Math.trunc(result)).toBe(expectedCompoundInterest);
-  });
-
-  it('【合算】初期資産:0万, 積立額:3万, 積立年数:3年', () => {
-    const initialAssetManYen = 0;
-    const monthlyAmounts = [30000];
-    const monthlyYears = [36];
-    const expectedSimpleInterest = 1080000;
-
+  // biome-ignore format: 一行にまとめたいため
+  it.each(mockSimpleInterestCalcInput)('simpleInterestCalc', ({ initialAssetManYen, monthlyAmounts, monthlyYears, expected }) => {
     const result = service['simpleInterestCalc'](initialAssetManYen, monthlyAmounts, monthlyYears);
-
-    expect(result).toBe(expectedSimpleInterest);
+    expect(Math.trunc(result)).toBe(expected);
   });
 
-  it('【複利計算】年利:5%, 初期資産:115万, 積立額:3万, 積立年数:3年', () => {
-    const initialAssetManYen = 1150000;
-    const monthlyAmounts = [30000];
-    const monthlyYears = [36];
-    const expectedCompoundInterest = 2491947;
+  const monthlyRate = 1.05 ** (1 / 12);
+  const mockCompoundInterestCalcInput = [
+    {
+      initialAssetManYen: 0,
+      monthlyAmounts: [50000],
+      monthlyYears: [12],
+      expected: 613628,
+    },
+    {
+      initialAssetManYen: 0,
+      monthlyAmounts: [50000, 60000],
+      monthlyYears: [12, 24],
+      expected: 2186052,
+    },
+    {
+      initialAssetManYen: 0,
+      monthlyAmounts: [50000, 60000, 70000],
+      monthlyYears: [12, 24, 36],
+      expected: 5238880,
+    },
+    {
+      initialAssetManYen: 0,
+      monthlyAmounts: [50000, 60000, 70000, 80000],
+      monthlyYears: [12, 24, 36, 48],
+      expected: 10599599,
+    },
+  ];
 
+  // biome-ignore format: 一行にまとめたいため
+  it.each(mockCompoundInterestCalcInput)('compoundInterestCalc', ({ initialAssetManYen, monthlyAmounts, monthlyYears, expected }) => {
     const result = service['compoundInterestCalc'](initialAssetManYen, monthlyAmounts, monthlyYears, monthlyRate);
-
-    expect(Math.trunc(result)).toBe(expectedCompoundInterest);
+    expect(Math.trunc(result)).toBe(expected);
   });
 
-  it('【合算】初期資産:115万, 積立額:3万, 積立年数:3年', () => {
-    const initialAssetManYen = 1150000;
-    const monthlyAmounts = [30000];
-    const monthlyYears = [36];
-    const expectedSimpleInterest = 2230000;
-
-    const result = service['simpleInterestCalc'](initialAssetManYen, monthlyAmounts, monthlyYears);
-
-    expect(result).toBe(expectedSimpleInterest);
-  });
-
-  it('【複利計算】年利:5%, 初期資産:0万, 積立額:3万 4万 5万, 積立年数:3年 5年 8年', () => {
-    const initialAssetManYen = 0;
-    const monthlyAmounts = [30000, 40000, 50000];
-    const monthlyYears = [36, 60, 96];
-    const expectedCompoundInterest = 12055913;
-
-    const result = service['compoundInterestCalc'](initialAssetManYen, monthlyAmounts, monthlyYears, monthlyRate);
-
-    expect(Math.trunc(result)).toBe(expectedCompoundInterest);
-  });
-
-  it('【合算】初期資産:0万, 積立額:3万 4万 5万, 積立年数:3年 5年 8年', () => {
-    const initialAssetManYen = 0;
-    const monthlyAmounts = [30000, 40000, 50000];
-    const monthlyYears = [36, 60, 96];
-    const expectedSimpleInterest = 8280000;
-
-    const result = service['simpleInterestCalc'](initialAssetManYen, monthlyAmounts, monthlyYears);
-
-    expect(result).toBe(expectedSimpleInterest);
-  });
-
-  it('【複利計算】年利:5%, 初期資産:115万, 積立額:3万 4万 5万, 積立年数:3年 5年 8年', () => {
-    const initialAssetManYen = 1150000;
-    const monthlyAmounts = [30000, 40000, 50000];
-    const monthlyYears = [36, 60, 96];
-    const expectedCompoundInterest = 14566219;
-
-    const result = service['compoundInterestCalc'](initialAssetManYen, monthlyAmounts, monthlyYears, monthlyRate);
-
-    expect(Math.trunc(result)).toBe(expectedCompoundInterest);
-  });
-
-  it('【合算】初期資産:115万, 積立額:3万 4万 5万, 積立年数:3年 5年 8年', () => {
-    const initialAssetManYen = 1150000;
-    const monthlyAmounts = [30000, 40000, 50000];
-    const monthlyYears = [36, 60, 96];
-    const expectedSimpleInterest = 9430000;
-
-    const result = service['simpleInterestCalc'](initialAssetManYen, monthlyAmounts, monthlyYears);
-
-    expect(result).toBe(expectedSimpleInterest);
-  });
-
-  it('【複利計算】年利:5%, 初期資産:0万, 積立額:2万 3万 4万 5万, 積立年数:1年 2年 3年 3年', () => {
-    const initialAssetManYen = 0;
-    const monthlyAmounts = [20000, 30000, 40000, 50000];
-    const monthlyYears = [12, 24, 36, 36];
-    const expectedCompoundInterest = 5100072;
-
-    const result = service['compoundInterestCalc'](initialAssetManYen, monthlyAmounts, monthlyYears, monthlyRate);
-
-    expect(Math.trunc(result)).toBe(expectedCompoundInterest);
-  });
-
-  it('【合算】初期資産:0万, 積立額:2万 3万 4万 5万, 積立年数:1年 2年 3年 3年', () => {
-    const initialAssetManYen = 0;
-    const monthlyAmounts = [20000, 30000, 40000, 50000];
-    const monthlyYears = [12, 24, 36, 36];
-    const expectedSimpleInterest = 4200000;
-
-    const result = service['simpleInterestCalc'](initialAssetManYen, monthlyAmounts, monthlyYears);
-
-    expect(result).toBe(expectedSimpleInterest);
-  });
-
-  it('【複利計算】年利:5%, 初期資産:115万, 積立額:2万 3万 4万 5万, 積立年数:1年 2年 3年 3年', () => {
-    const initialAssetManYen = 1150000;
-    const monthlyAmounts = [20000, 30000, 40000, 50000];
-    const monthlyYears = [12, 24, 36, 36];
-    const expectedCompoundInterest = 6884099;
-
-    const result = service['compoundInterestCalc'](initialAssetManYen, monthlyAmounts, monthlyYears, monthlyRate);
-
-    expect(Math.trunc(result)).toBe(expectedCompoundInterest);
-  });
-
-  it('【合算】初期資産:115万, 積立額:2万 3万 4万 5万, 積立年数:1年 2年 3年 3年', () => {
-    const initialAssetManYen = 1150000;
-    const monthlyAmounts = [20000, 30000, 40000, 50000];
-    const monthlyYears = [12, 24, 36, 36];
-    const expectedSimpleInterest = 5350000;
-
-    const result = service['simpleInterestCalc'](initialAssetManYen, monthlyAmounts, monthlyYears);
-
-    expect(result).toBe(expectedSimpleInterest);
-  });
-
-  it('年利:5%, 初期資産:0万, 積立額:3万 4万 5万, 積立年数:3年 5年 8年', () => {
-    const input: Input = {
+  // biome-ignore format: 一行にまとめたいため
+  type MockTsumitateEasyCalculateInput = Input & { expected: { compoundInterest: number; diff: number; simpleInterest: number; } };
+  const mockTsumitateEasyCalculateInput: MockTsumitateEasyCalculateInput[] = [
+    {
       initialAsset: 0,
-      amounts: [3, 4, 5],
-      years: [3, 5, 8],
+      amounts: [5],
+      years: [1],
       rate: 5,
-    };
-
-    const expectedCompoundInterest = 12055913;
-    const expectedSimpleInterest = 8280000;
-    const expectedDiff = expectedCompoundInterest - expectedSimpleInterest;
-
-    const result = service.tsumitateEasyCalculate(input);
-
-    expect(Math.trunc(result.compoundInterestCalcResult)).toBe(expectedCompoundInterest);
-    expect(result.simpleInterestCalcResult).toBe(expectedSimpleInterest);
-    expect(Math.trunc(result.diff)).toBe(expectedDiff);
-  });
-
-  it('年利:5%, 初期資産:115万, 積立額:3万 4万 5万, 積立年数:3年 5年 8年', () => {
-    const input: Input = {
-      initialAsset: 115,
-      amounts: [3, 4, 5],
-      years: [3, 5, 8],
+      expected: {
+        compoundInterest: 613628,
+        diff: 13628,
+        simpleInterest: 600000,
+      },
+    },
+    {
+      initialAsset: 0,
+      amounts: [5, 6],
+      years: [1, 2],
       rate: 5,
-    };
+      expected: {
+        compoundInterest: 2186052,
+        diff: 146052,
+        simpleInterest: 2040000,
+      },
+    },
+    {
+      initialAsset: 0,
+      amounts: [5, 6, 7],
+      years: [1, 2, 3],
+      rate: 5,
+      expected: {
+        compoundInterest: 5238880,
+        diff: 678880,
+        simpleInterest: 4560000,
+      },
+    },
+    {
+      initialAsset: 0,
+      amounts: [5, 6, 7, 8],
+      years: [1, 2, 3, 4],
+      rate: 5,
+      expected: {
+        compoundInterest: 10599599,
+        diff: 2199599,
+        simpleInterest: 8400000,
+      },
+    },
+  ];
 
-    const expectedCompoundInterest = 14566219;
-    const expectedSimpleInterest = 9430000;
-    const expectedDiff = expectedCompoundInterest - expectedSimpleInterest;
+  // biome-ignore format: 一行にまとめたいため
+  it.each(mockTsumitateEasyCalculateInput)('tsumitateEasyCalculate', ({ initialAsset, amounts, years, rate, expected }) => {
+    const result = service.tsumitateEasyCalculate({ initialAsset, amounts, years, rate });
 
-    const result = service.tsumitateEasyCalculate(input);
-
-    expect(Math.trunc(result.compoundInterestCalcResult)).toBe(expectedCompoundInterest);
-    expect(result.simpleInterestCalcResult).toBe(expectedSimpleInterest);
-    expect(Math.trunc(result.diff)).toBe(expectedDiff);
+    expect(Math.trunc(result.compoundInterestCalcResult)).toBe(expected.compoundInterest);
+    expect(result.simpleInterestCalcResult).toBe(expected.simpleInterest);
+    expect(Math.trunc(result.diff)).toBe(expected.diff);
   });
 });
